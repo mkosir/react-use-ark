81920cada9ba25f4fbf2d8a9164bdb44dfff9def
mkosir <kosir.mar@gmail.com>
2019-12-09 16:39:57 +0100

    update storybook config

---
+++ .storybook/config-theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming/create';
+import logo from './sb_logo.png';
+
+export default create({
+  base: 'light',
+
+  brandTitle: 'React Use ARK Ѧ',
+  brandUrl: 'https://github.com/mkosir/react-use-ark',
+  brandImage: logo,
+});
